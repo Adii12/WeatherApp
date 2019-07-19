17602014c37551040dc865988cc237eae969291c
Adii12 <cimpean_adrian12@yahoo.com>
2019-07-19 13:55:46 +0300

    Started function to connect to API. Changed app launch icon.

--- app/src/main/java/com/example/weatherapp/Function.java
+++ app/src/main/java/com/example/weatherapp/Function.java
@@ -21,14 +21,17 @@ public class Function {
             return false;
     }
 
+
     //TO DO :::
-    public static void executGet(String targetURL) {
+    public static void fetchData(String city) {
+        final String API_KEY="2397cac5640f1ba782245157aab0343b";
+
 
         URL url;
         HttpURLConnection connection = null;
 
         try {
-            url = new URL(targetURL);
+            url = new URL("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+API_KEY);
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestProperty("content-type", "application/json; charset=utf-8");
             connection.setRequestProperty("Content-Language", "en-US");
@@ -36,6 +39,7 @@ public class Function {
             connection.setDoInput(true);
             connection.setDoOutput(false);
 
+            InputStream is;
 
         }catch (Exception ex){
             //return null;
@@ -46,6 +50,50 @@ public class Function {
         }
 
 
+    }
+
+    public static String setIcon(int actualId, long sunrise, long sunset){
+        int id = actualId/100;
+        String icon="";
+
+        if(actualId==800){
+            long currentTime=new Date().getTime();
+            if(currentTime>=sunrise && currentTime<sunset){
+                icon = "&xf00d";
+            }
+            else{
+                icon= "&#xf02e";
+            }
+        }
+        else{
+            switch (id){
+                case 2:
+                    icon="&#xf01e";
+                    break;
+
+                case 3:
+                    icon="&#xf01e";
+                    break;
+
+                case 5:
+                    icon="&#xf019";
+                    break;
+
+                case 6:
+                    icon="&#xf01b";
+                    break;
+
+                case 7:
+                    icon="&#xf01c";
+                    break;
+
+                case 8:
+                    icon="&#xf014";
+                    break;
+            }
+        }
+
+        return icon;
     }
 
 
--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.TextView;
 import android.graphics.Typeface;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView weatherIcon;
-    Typeface weatherFont = Typeface.createFromAsset(getAssets(),"res/font/icons.ttf");
+    TextView weatherIcon, currentTemperature;
+    Typeface weatherFont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +20,16 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide(); //face aplicatia fullscreen
         setContentView(R.layout.activity_main);
 
-        weatherIcon=(TextView) findViewById(R.id.weather_icon);
+        if(Function.isNetworkAvailable(getApplicationContext())==false)
+            Toast.makeText(getApplicationContext(),"No Internet Connection", Toast.LENGTH_LONG).show();
+
+        weatherFont = getResources().getFont(R.font.icons);
+        weatherIcon=findViewById(R.id.weather_icon);
         weatherIcon.setTypeface(weatherFont);
 
-        weatherIcon.setText("&#xf00d");
+        currentTemperature=findViewById(R.id.temperature_field);
+        weatherIcon.setText(Html.fromHtml(Function.setIcon(802,1485720272,1485766550)));
+        currentTemperature.setText(String.format("%.2f",28.514)+"*C");
 
     }
 }
